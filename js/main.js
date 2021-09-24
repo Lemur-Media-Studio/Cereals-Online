73baff1388d830ffa40794aafd3512760d76dcf2
Martin del Rosario <delrosariomartin23@gmail.com>
2021-09-23 23:57:28 -0400

    termine animacion y queries

--- js/main.js
+++ js/main.js
@@ -8,3 +8,28 @@ window.addEventListener('load', () => {
         console.log('BODY CARGADO')
     });
 })
+
+$('.counter').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+    
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+  
+    {
+      duration: 5148,
+      easing:'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum+"%");
+        //alert('finished');
+      }
+  
+    });  
+    
+    
+  
+  });
